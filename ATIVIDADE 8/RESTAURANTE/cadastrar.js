ae72bef0c31c163d0baf30976b37e1d82624e078
Maíra Figueiredo <mairabh94@yahoo.com.br>
2024-11-28 18:52:32 -0300

    Add files via upload

---
+++ ATIVIDADE 8/RESTAURANTE/cadastrar.js
@@ -0,0 +1,8 @@
+async function cadastrarComida(){
+const nomeDigitado = document.querySelector('#nome').value
+const precoDigitado = document.querySelector('#preco').value
+const descricaoDigitado = document.querySelector('#descricao').value
+const imagemDigitado = document.querySelector('#imagem').value
+
+
+}    
