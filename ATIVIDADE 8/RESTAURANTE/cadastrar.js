da70e27b1e9b8e855e5fd464c78fbf3f44207a3f
Maíra Figueiredo <mairabh94@yahoo.com.br>
2024-12-05 17:47:09 -0300

    inclusão de botão pra exclusão de comida
    alteração conforme a aula

--- ATIVIDADE 8/RESTAURANTE/cadastrar.js
+++ ATIVIDADE 8/RESTAURANTE/cadastrar.js
@@ -1,8 +1,24 @@
 async function cadastrarComida(){
 const nomeDigitado = document.querySelector('#nome').value
 const precoDigitado = document.querySelector('#preco').value
-const descricaoDigitado = document.querySelector('#descricao').value
-const imagemDigitado = document.querySelector('#imagem').value
+const descricaoDigitada = document.querySelector('#descricao').value
+const imagemDigitada = document.querySelector('#imagem').value
 
+
+const url = 'https://6748c2735801f51535921495.mockapi.io/api/cardapio'
+const dadosComidaNova = {
+    nome: nomeDigitado, 
+    descricao: descricaoDigitada,
+    preco: precoDigitado,
+    img: imagemDigitada
+}
+
+const resposta = await fetch(url,{
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(dadosComidaNova)
+})
+
+alert('Comida cadastrada com sucesso !')
 
 }    
--- ATIVIDADE 8/RESTAURANTE/script.js
+++ ATIVIDADE 8/RESTAURANTE/script.js
@@ -3,6 +3,7 @@ async function carregarCardapio(){
     const resposta = await fetch(url)
     const dadosCardapio = await resposta.json()
     const sectionMenu = document.querySelector('#menu')
+    sectionMenu.innerHTML = ''
     dadosCardapio.forEach(comida => {
         const estruturaHTMLComida = `
 <div class="item">
@@ -11,10 +12,20 @@ async function carregarCardapio(){
         <h2> ${comida.nome}</h2>
         <p> ${comida.descricao}</p>
         <span> R$ ${comida.preco}</span>
+        <button onclick="excluirComida('${comida.id}')"> Excluir comida </button>
     </div>
 </div>`
     sectionMenu.innerHTML += estruturaHTMLComida
     })
+}
+
+async function excluirComida(id) {
+    const url = `https://6748c2735801f51535921495.mockapi.io/api/cardapio/${id}`
+    const resposta = fetch(url,{
+        method: 'DELETE'
+    })
+    alert('Comida excluída!')
+    
 }
 
 carregarCardapio()
